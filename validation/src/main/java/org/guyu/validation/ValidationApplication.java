0820a0f20f8b908dc6fb9fa056ed248bf595c260
guyu1 <guyu1@vipkid.com.cn>
2019-08-16 15:46:27 +0800

    update

---
+++ validation/src/main/java/org/guyu/validation/ValidationApplication.java
@@ -0,0 +1,15 @@
+package org.guyu.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guyu
+ * created at 2019/7/22 5:43 PM
+ */
+@SpringBootApplication
+public class ValidationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApplication.class, args);
+    }
+}
